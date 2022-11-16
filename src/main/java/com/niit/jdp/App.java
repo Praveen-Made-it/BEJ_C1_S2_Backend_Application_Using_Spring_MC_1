7066adbb7a8487efa1345397933c8a530bca2c12
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 14:12:08 +0530

    final commit

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -13,12 +13,19 @@ public class App {
          a parameter.*/
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MovieConfig.class);
 
+
+        /* Getting the bean with the name `movie1` from the `ApplicationContext` and casting it
+         to the `Movie` class. */
         Movie movie1 = context.getBean("movie1", Movie.class);
         System.out.println("Movie1: " + movie1);
 
+        /* Getting the bean with the name `movie2` from the `ApplicationContext` and casting it
+         to the `Movie` class.*/
         Movie movie2 = context.getBean("movie2", Movie.class);
         System.out.println("Movie2: " + movie2);
 
+       /* Getting the bean with the name `movie3` from the `ApplicationContext` and casting it
+        to the `Movie` class.*/
         Movie movie3 = context.getBean("movie3", Movie.class);
         System.out.println("Movie3: " + movie3);
 
