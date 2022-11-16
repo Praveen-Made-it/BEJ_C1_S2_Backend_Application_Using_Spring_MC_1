b331705551135e0516d188e2b7dcc54872b959c9
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 14:10:22 +0530

    calling the all movies inside the main method

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,6 +1,7 @@
 package com.niit.jdp;
 
 import com.niit.jdp.config.MovieConfig;
+import com.niit.jdp.domain.Movie;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -11,6 +12,15 @@ public class App {
         /* Creating a new instance of the `AnnotationConfigApplicationContext` class and passing the `MovieConfig` class as
          a parameter.*/
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MovieConfig.class);
+
+        Movie movie1 = context.getBean("movie1", Movie.class);
+        System.out.println("Movie1: " + movie1);
+
+        Movie movie2 = context.getBean("movie2", Movie.class);
+        System.out.println("Movie2: " + movie2);
+
+        Movie movie3 = context.getBean("movie3", Movie.class);
+        System.out.println("Movie3: " + movie3);
 
     }
 }
