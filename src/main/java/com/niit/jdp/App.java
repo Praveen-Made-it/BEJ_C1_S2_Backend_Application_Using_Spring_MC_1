e79627e9c1bfe69aeb03ae651d4594f82afba5ad
Praveen Kumar <praveen170596@gmail.com>
2022-11-16 14:04:42 +0530

    Creating a new instance of the `AnnotationConfigApplicationContext` class and passing the `MovieConfig` class as a parameter.

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,10 +1,16 @@
 package com.niit.jdp;
 
+import com.niit.jdp.config.MovieConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /* Creating a new instance of the `AnnotationConfigApplicationContext` class and passing the `MovieConfig` class as
+         a parameter.*/
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MovieConfig.class);
+
     }
 }
